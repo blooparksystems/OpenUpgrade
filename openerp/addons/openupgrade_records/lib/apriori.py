72455661a4de426a1159779fc4b01dd48956dd9a
Christoph Giesel <cgiesel@bloopark.de>
2015-01-20 14:02:03 +0100

    added custom module renamings

--- openerp/addons/openupgrade_records/lib/apriori.py
+++ openerp/addons/openupgrade_records/lib/apriori.py
@@ -5,6 +5,9 @@ to help the matching process
 renamed_modules = {
     'base_calendar': 'calendar',
     'mrp_jit': 'procurement_jit',
+    #leaserad
+    'profile_leaserad': 'lr_report',
+    'email_cc_bcc': 'lr_mail',
     }
 
 renamed_models = {
